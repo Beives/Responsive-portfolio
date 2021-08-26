49487940dd61b11714ce34f580e8383c798a3fd0
Beives <b.bakos98@gmail.com>
2021-08-26 16:00:42 +0200

    added project category selection

---
+++ dist/js/work.js
@@ -0,0 +1,25 @@
+let currentActive = document.querySelector('.active');
+let currentSelection = "";
+
+const buttons = document.querySelectorAll('.selectBtn').forEach(button =>{
+    button.addEventListener('click', event=>{
+        currentActive.classList.remove('active');
+        currentActive = event.target;
+        event.target.classList.add('active');
+        currentSelection = event.target.dataset.btn;
+        changeVisibility();
+    })
+});
+
+const projects = document.querySelectorAll('.item');
+
+function changeVisibility(){
+    projects.forEach(el => {
+        if(el.dataset.type == currentSelection){
+            el.style.display = 'none';
+        }
+        else{
+            el.style.display = 'block';
+        }
+    });
+}
